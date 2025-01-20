22873040fd44da04d9198093c9425ed2c949c570
F95875C <matteo.filippini@ivecogroup.com>
2025-01-20 14:08:23 +0100

    Aggiunta gestione sigole stese

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import stese
 
 if __name__ == "__main__":
-    # Nome del file JSON contenente il mazzo di tarocchi
-    file_mazzo = "mazzo_tarocchi.json"  
     # Implementare menù gestione stese
-    stese.menu_stesa(file_mazzo)
-    
+    stese.menu_stesa()
